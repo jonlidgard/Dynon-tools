49eed54201f442f078e97bc601ff5f54a0d10d6e
Jon Lidgard <jonlidgard@gmail.com>
2024-08-28 20:37:07 +0100

    Refactored to use traits, code still messy - need to look at generics next

--- src/dynon_data_defs.rs
+++ src/dynon_data_defs.rs
@@ -1,5 +1,50 @@
+#![allow(unused)]
+#[cfg(test)]
 
-struct ADAHRSData {
+use core::{str, time};
+
+
+const D1X0_EMS_FRAME_LENGTH: usize = 121;
+const D1X0_EFIS_FRAME_LENGTH: usize = 53;
+const SYSTEM_TIME_LENGTH: usize = 8;
+
+type D1x0EmsFrame = [u8; D1X0_EMS_FRAME_LENGTH];
+type D1x0EfisFrame = [u8; D1X0_EFIS_FRAME_LENGTH];
+
+pub trait DynonSerializable {
+    fn calc_crc(data: &[u8]) -> u8 {
+        let sum: u64 = data[..data.len()-4].iter().map(|x| *x as u64).sum();
+        (sum & 0xff) as u8
+    }
+
+    fn add_crc_crlf(crc: u8, data: &mut[u8]) {
+        let l = data.len();
+
+        // CRC
+        let crc_str = format!("{crc:02X}");
+        //println!("CRC: {:?}, {:?}",crc, crc_str);
+        data[l-4 .. l-2].copy_from_slice(crc_str.as_bytes());
+
+        // CRLF        
+        data[l-1] = b'\n';
+        data[l-2] = b'\r';
+    }
+
+    fn serialize(&self) -> DynonSerializedData;
+}
+
+
+pub enum DynonSerializedData {
+//    Adahrs,
+//    System,
+//    EMS,
+    D1x0Efis(D1x0EfisFrame),
+    D1x0Ems(D1x0EmsFrame),
+}
+
+
+
+/*struct ADAHRSData {
     system_time: u32,  // HHMMSSFF
     pitch: i16,  // unit: deg / n * 10 / positive = pitch up / XXXX is unavailable
     roll: f32,  // unit: deg / n * 10 / positive = right bank / XXXXX
@@ -107,17 +152,18 @@ struct EMSData {
     power_percent: i16,  // 0-200 / unit: % / XXX
     egt_leaning_status: char  // L=lean of peak P=peak R:rich of peak X: not available / (1)
 }
+*/
 
-
-struct D1x0EMSData {
+#[derive(Debug)]
+pub struct D1x0EMSData {
     system_time: u32,  // HHMMSSFF
     manifold_pressure: f32,  // unit: inHg * 100 (4)
     oil_temp: i16,  // unit : deg celcius / XXX (3)
-    oil_pressure: i16,  // unit : psi / (3)
+    oil_pressure: u16,  // unit : psi / (3)
     fuel_pressure: f32,  // unit: PSI / n * 10 / XXX (3)
     volts: f32,  // n * 10 / (3)
     amps: f32,  // n * 10 / (3)
-    rpm: i16,  // n / 10 (3)
+    rpm: u16,  // n / 10 (3)
     fuel_flow: f32,  // unit: gallons per hour / n * 10 / (3)
     fuel_remaining: f32,  // unit: gallons / calculated by fuel computer / n * 10 / XXX (4)
     fuel_level_1: f32,  // unit: gallons / if MAIN tank exists, it will be MAIN / n * 10 / XXX (3)
@@ -138,16 +184,93 @@ struct D1x0EMSData {
     cht_4: i16, //(3)
     cht_5: i16, //(3)
     cht_6: i16, //(3)
-    contact_1: i16, //(1)
-    contact_2: i16, //(2)
+    contact_1: u16, //(1)
+    contact_2: u16, //(2)
 }
 
 
-struct D1x0EFISData {
+impl D1x0EMSData {
+    pub fn new(time_count: u32, freq: Option<u32>) -> Self {
+        let f = freq.unwrap_or(100);
+        let phase: f32 = (( time_count as f32 % f as f32 ) * 180.0 / f as f32).to_radians();
+        //println!("Phase: {}", phase);
+        Self {
+            system_time: time_count,
+            manifold_pressure: 0.0,
+            oil_temp: 0,
+            oil_pressure: (50.0 * f32::sin(phase)) as u16,
+            fuel_pressure: 0.0,
+            volts: 12.0,
+            amps: 5.0,
+            rpm: (3000.0 * f32::sin(phase)) as u16,
+            fuel_flow: 0.0,
+            fuel_remaining: 0.0,
+            fuel_level_1: 0.0,
+            fuel_level_2: 0.0,
+            gp_1: ['X'; 8],
+            gp_2: ['X'; 8],
+            gp_3: ['X'; 8],
+            gp_thermocouple: 0,
+            egt_1: 0,
+            egt_2: 0,
+            egt_3: 0,
+            egt_4: 0,
+            egt_5: 0,
+            egt_6: 0,
+            cht_1: 0,
+            cht_2: 0,
+            cht_3: 0,
+            cht_4: 0,
+            cht_5: 0,
+            cht_6: 0,
+            contact_1: 0,
+            contact_2: 0
+        }
+    }
+}
+
+impl DynonSerializable for D1x0EMSData {
+    fn calc_crc(data: &[u8]) -> u8 {
+        let sum: u64 = data[..data.len()-4].iter().map(|x| *x as u64).sum();
+        (0x100 - (sum & 0xff)) as u8
+    }
+
+    fn serialize(&self) -> DynonSerializedData {
+        
+        let mut data:D1x0EmsFrame = [b'0'; D1X0_EMS_FRAME_LENGTH];
+        
+        // system_time
+        let time_string = format!("{:0>8}", self.system_time.to_string());
+        data[..SYSTEM_TIME_LENGTH].copy_from_slice(time_string.as_bytes());
+       
+        // oil pressure
+        let oil_pressure = format!("{:0>3}", self.oil_pressure.to_string());
+        data[15 .. 18].copy_from_slice(oil_pressure.as_bytes());
+
+        // rpm
+        let rpm = format!("{:0>3}", (self.rpm / 10).to_string());
+        data[27 .. 30].copy_from_slice(rpm.as_bytes());
+
+        // GP1-3 as unused 'XXXXXXXX'
+        data[43 .. 67].copy_from_slice("XXXXXXXXXXXXXXXXXXXXXXXX".as_bytes());
+
+        // crc
+        let crc = Self::calc_crc(&data);
+        
+        Self::add_crc_crlf(crc, &mut data);
+        DynonSerializedData::D1x0Ems(data)
+    }
+}
+
+
+//---------------------------
+
+#[derive(Debug)]
+pub struct D1x0EFISData {
     system_time: u32,  // HHMMSSFF
     pitch: f32,  // unit: deg / n * 10 / positive = pitch up / XXXX is unavailable
     roll: f32,  // unit: deg / n * 10 / positive = right bank / XXXXX
-    yaw: i16,  // unit: deg / XXX
+    yaw: u16,  // unit: deg / XXX
     ias: f32,  // IAS / unit: m/s / n * 10 / XXXX
 //    pressure_alt: i16,  // unit : feet / at 29.92Hg / can be minus (sea level) / XXXXXX
 //    displayed_alt: i16,
@@ -155,5 +278,117 @@ struct D1x0EFISData {
 //    vertical_speed: f32,  // unit: ft/min / positive = climbing / XXX(4)
     lateral_accel: f32,  // unit: g / n * 100 / positive = left skid (ball right) / XXX
     vertical_accel: f32,  // unit: g / n * 100 / positive = going up accel / XXX
-    aoa: i16,  // AOA / unit: % / XX
+    aoa: u16,  // AOA / unit: % / XX
+}
+
+impl D1x0EFISData {
+    pub fn new(time_count: u32, freq: Option<u32>) -> Self {
+        let f = freq.unwrap_or(100);
+        let phase: f32 = (( time_count as f32 % f as f32 ) * 360.0 / f as f32).to_radians();
+        //println!("Phase: {}", phase);
+        Self {
+            system_time: time_count,
+            pitch: (25.0 * f32::sin(phase)),
+            roll: (60.0 * f32::sin(phase)),
+            yaw: (90.0 * f32::sin(phase)) as u16 + 180,
+            ias: 0.0,
+            lateral_accel: 0.0,
+            vertical_accel: 1.0,
+            aoa: 0
+        }
+    }
+}
+
+impl DynonSerializable for D1x0EFISData {
+
+    fn serialize(&self) -> DynonSerializedData {
+        
+        let mut data:D1x0EfisFrame = [b'0'; D1X0_EFIS_FRAME_LENGTH];
+        
+        // system_time
+        let time_string = format!("{:0>8}", self.system_time.to_string());
+        data[..SYSTEM_TIME_LENGTH].copy_from_slice(time_string.as_bytes());
+       
+        // pitch
+        data[8] = if self.pitch < 0.0  { b'-' } else { b'+' }; 
+        let pitch = format!("{:0>3}", ((self.pitch * 10.0).abs() as u32).to_string());
+        //println!("Pitch {:?}", pitch.as_bytes());
+        data[9 .. 12].copy_from_slice(pitch.as_bytes());
+
+        // roll
+        data[12] = if self.roll < 0.0  { b'-' } else { b'+' }; 
+        let roll = format!("{:0>4}", ((self.roll * 10.0).abs() as u32).to_string());
+        data[13 .. 17].copy_from_slice(roll.as_bytes());
+
+        // // yaw
+        // let yaw = format!("{:0>3}", self.yaw.to_string());
+        // data[18 .. 21].copy_from_slice(roll.as_bytes());
+
+       
+        // crc
+        let crc = Self::calc_crc(&data);
+        
+        Self::add_crc_crlf(crc, &mut data);
+        DynonSerializedData::D1x0Efis(data)
+    }
+}
+
+//--------------------------------
+
+#[test]
+fn test_d1x0_efis_calc_crc() {
+    let mut x: D1x0EfisFrame = [0; D1X0_EFIS_FRAME_LENGTH];
+    x.copy_from_slice("00082119+058-00541301200+9141+011-01+15003EA0C701A400".as_bytes());
+     println!("X: {:?}", x);
+    let y = D1x0EFISData::calc_crc(&x);
+    //println!("Y: {:?}",y);
+    assert_eq!(y,0xA4);
+}
+
+
+#[test]
+fn test_d1x0_ems_calc_crc() {
+    let mut x: D1x0EmsFrame = [0; D1X0_EMS_FRAME_LENGTH];
+    x.copy_from_slice("0012224826351340262441240122631320562191191OAT00090TRE-0061FLP0001020481378139214061421143514503583533633743843951103D200".as_bytes());
+
+    let y = D1x0EMSData::calc_crc(&x);
+    //println!("Y: {:?}",y);
+    assert_eq!(y,0xD2);
+}
+
+
+#[test]
+fn test_d1x0_efis_serialize() {
+    let x = D1x0EFISData::new(12345678, None);
+    println!("X: {:?}", x);
+    let d = x.serialize();
+
+    match d {
+        DynonSerializedData::D1x0Efis(y) => {
+            println!("Y: {:?}",str::from_utf8(&y));
+            assert_eq!(&y[..8], "12345678".as_bytes());
+            let l = y.len();
+            assert_eq!(&y[l-4 .. l-2], "6F".as_bytes());
+        }      
+        _ => panic!("Returned wrong type."),
+    }
+
+    
+}
+
+
+#[test]
+fn test_d1x0_ems_serialize() {
+    let x = D1x0EMSData::new(12345678, None);
+    println!("X: {:?}", x);
+    let d = x.serialize();
+    match d {
+        DynonSerializedData::D1x0Ems(y) => {
+            println!("Y: {:?}",str::from_utf8(&y));
+            assert_eq!(&y[..8], "12345678".as_bytes());
+            let l = y.len();
+            assert_eq!(&y[l-4 .. l-2], "1D".as_bytes());
+        }
+        _ => panic!("Returned wrong type."),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,158 @@
+#![allow(unused)]
+use core::str;
+use std::io::{self, Write};
+use std::process::exit;
+use std::time::Duration;
+use std::thread;
+
+use clap::{Arg, Command};
+
+use dynon_data_defs::{DynonSerializable, DynonSerializedData};
+use serialport::{DataBits, SerialPort, StopBits};
+
+mod dynon_data_defs;
+
+const EFIS_DATA_OPTION: &str = "efis";
+const EMS_DATA_OPTION: &str = "ems";
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Serialport Example - Heartbeat")
+        .about("Write dummy efis or ems data frames to a serial port at 16Hz")
+        .disable_version_flag(true)
+        .arg(
+            Arg::new("port")
+                .help("The device path to a serial port")
+                .required(true),
+        )
+        .arg(
+            Arg::new("type")
+                .value_parser([EFIS_DATA_OPTION, EMS_DATA_OPTION])
+                .default_value("ems")
+                .help("The type of data to send - ems or efis")
+        ).arg(
+            Arg::new("baud")
+                .long("baud")
+                .help("The baud rate to connect at")
+                .use_value_delimiter(false)
+                .default_value("115200")
+                .value_parser(clap::value_parser!(u32).range(9600..)),
+        )
+        .arg(
+            Arg::new("rate")
+                .long("rate")
+                .help("Frequency (Hz) to repeat transmission of the pattern (0 indicates sending only once")
+                .num_args(1)
+                .default_value("16")
+                .value_parser(clap::value_parser!(u32)),
+        )
+        .get_matches();
+
+    let port_name = matches.get_one::<String>("port").unwrap();
+    let data_type: &String = matches.get_one::<String>("type").expect("default");
+    let baud_rate = matches.get_one::<u32>("baud").unwrap(); //.parse::<u32>().unwrap();
+    let stop_bits = StopBits::One;
+    let data_bits = DataBits::Eight;
+    let rate = matches.get_one::<u32>("rate").unwrap(); //.parse::<u32>().unwrap();
+    
+    //println!("Available ports: {:#?}", serialport::available_ports());
+    let builder = serialport::new(  port_name, *baud_rate)
+        .stop_bits(stop_bits)
+        .data_bits(data_bits);
+    //println!("{:?}", &builder);
+
+    let mut port = builder.open().unwrap_or_else(|e| {
+        eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+        ::std::process::exit(1);
+    });
+
+    println!(
+        "Sending Dynon D1X0 {} data to {} at {} baud at {}Hz",
+        &data_type.to_uppercase(), &port_name, &baud_rate, &rate
+    );
+
+    /*
+        Idea is to generate some serial data, output at a user-defined rate in order to test out the datalogger.
+        Time will increment by 1 each transmission to allow detection of missing frames.
+        rpm & oil pressure will vary sinusoidally over 100 frames
+        everything else can remain static for now.    
+     */
+
+    let mut frame_time = 1;
+    //let mut ems_frame: dynon_data_defs::D1x0EMSData;
+
+    match data_type.as_str() {
+        EFIS_DATA_OPTION => send_efis(port, rate),
+        EMS_DATA_OPTION => send_ems(port, rate),
+        _ => { eprintln!("Invalid type specified: use either efis or ems"); ::std::process::exit(1);}
+    }
+        
 }
+
+fn valid_baud(val: &str) -> std::result::Result<(), String> {
+    val.parse::<u32>()
+        .map(|_| ())
+        .map_err(|_| format!("Invalid baud rate '{}' specified", val))
+}
+
+fn send_ems(mut port: Box<dyn SerialPort>, rate: &u32) {
+    let mut frame_time = 1;
+    let r = *rate;
+//    thread::spawn( move || {
+        
+    loop {
+        let ems_frame = dynon_data_defs::D1x0EMSData::new(frame_time, Some(100));
+        frame_time +=1;
+        let dynon_serialized_data = &ems_frame.serialize();
+        match dynon_serialized_data {
+            DynonSerializedData::D1x0Ems(bytes) => {
+                match port.write( &bytes[0..]) {
+                    Ok(_) => {
+                        //print!("{}", byte_string);
+                        std::io::stdout().flush().unwrap();
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => {eprintln!("{:?}", e); ::std::process::exit(1);},
+                }
+                if r == 0 {
+                    return;
+                }
+            },
+            _ => (),
+        }
+
+        std::thread::sleep(Duration::from_millis((1000.0 / (r as f32)) as u64));
+    }
+  //      });
+
+}
+
+fn send_efis(mut port: Box<dyn SerialPort>, rate: &u32) {
+    let mut frame_time = 1;
+    let r = *rate;
+ //   thread::spawn( move || {
+    loop {
+        let efis_frame = dynon_data_defs::D1x0EFISData::new(frame_time, Some(100));
+        frame_time +=1;
+        let dynon_serialized_data = &efis_frame.serialize();
+        match dynon_serialized_data {
+            DynonSerializedData::D1x0Efis(bytes) => {
+                match port.write( &bytes[0..]) {
+                    Ok(_) => {
+                        //print!("{}", byte_string);
+                        std::io::stdout().flush().unwrap();
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => {eprintln!("{:?}", e); ::std::process::exit(1);},
+                }
+                if r == 0 {
+                    return;
+                }
+            },
+            _ => (),
+        }
+        std::thread::sleep(Duration::from_millis((1000.0 / (r as f32)) as u64));
+    }
+//     });
+}
+  
+    
