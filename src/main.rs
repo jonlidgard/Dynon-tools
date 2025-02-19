d71806a8a4c67038fafa6984a6eadd8e61a1430f
Jon Lidgard <jonlidgard@gmail.com>
2025-02-19 15:22:24 +0000

    Changed default transmission rate

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,21 @@ mod dynon_data_defs;
 const EFIS_DATA_OPTION: &str = "efis";
 const EMS_DATA_OPTION: &str = "ems";
 
+const ABOUT: &str = "
+
+Write dummy efis or ems data frames to a serial port.
+
+- Defauls to 115200 baud & 1Hz transmission rate which
+  is the Dynon EFIS D100 & EMS D120 max rate.
+
+- THE TIME VALUE NEEDS CHANGING TO A PURE INCREMENT NOT THE
+  FUNCTION CURRENTLY USED.
+";
+
+
 fn main() {
     let matches = Command::new("Serialport Example - Heartbeat")
-        .about("Write dummy efis or ems data frames to a serial port at 16Hz")
+        .about(ABOUT)
         .disable_version_flag(true)
         .arg(
             Arg::new("port")
@@ -42,7 +54,7 @@ fn main() {
                 .long("rate")
                 .help("Frequency (Hz) to repeat transmission of the pattern (0 indicates sending only once")
                 .num_args(1)
-                .default_value("16")
+                .default_value("1")
                 .value_parser(clap::value_parser!(u32)),
         )
         .get_matches();
@@ -65,8 +77,9 @@ fn main() {
         ::std::process::exit(1);
     });
 
-    println!(
-        "Sending Dynon D1X0 {} data to {} at {} baud at {}Hz",
+    println!("THE TIME VALUE NEEDS CHANGING TO A PURE INCREMENT NOT THE
+  FUNCTION CURRENTLY USED.\n \
+        Sending Dynon D1X0 {} data to {} at {} baud at {}Hz",
         &data_type.to_uppercase(), &port_name, &baud_rate, &rate
     );
 
